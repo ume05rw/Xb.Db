150cc06fa1a20c67019af3d88967b0adbec14305
ume05rw <info@dobes.jp>
2016-12-21 15:18:32 +0900

    説明文を修正。

--- Xb.Db.STD1.3/Properties/AssemblyInfo.cs
+++ Xb.Db.STD1.3/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("Xb.Db")]
-[assembly: AssemblyDescription("Ready to Xamarin & .NET Core, Base-Library for Database Access.")]
+[assembly: AssemblyDescription("Ready to Xamarin & .NET Core, Base-Library for Database Access & Simple implementation of Table-Model.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Do-Be's")]
 [assembly: AssemblyProduct("Xb.Db")]
